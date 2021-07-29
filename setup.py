679d84604e5e8ba02ebabdbfdf70fc3ec6d7679b
Iman Nematollahi <nematoli@informatik.uni-freiburg.de>
2021-07-29 18:24:50 +0200

    Added some dependencies to setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,8 @@ from setuptools import setup
 
 setup(name='robot_io',
       version='0.0.1',
-      install_requires=['numpy']
+      install_requires=['numpy',
+                        'scipy',
+                        'open3d',
+                        'hydra.core']
 )
