5a72e6691e208aeee1748aef19db20c25abf8b52
Oier Mees <oier.mees@gmail.com>
2021-08-06 12:45:21 +0200

    added threaded digit tactile sensor

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 This repository provides code to control the Franka Emika Panda and Kuka IIWA robots of AIS, University of Freiburg.
-Maintainers are Lukas Hermann, Max Argus, Iman Nematollahi and Adrian Röfer.
+Maintainers are Lukas Hermann, Max Argus, Iman Nematollahi, Oier Mees and Adrian Röfer.
 
 # [Setup](docs/setup.md)
 
--- robot_io/tactile_sensing/digits.py
+++ robot_io/tactile_sensing/digits.py
@@ -1,4 +1,7 @@
+import threading
+import time
 import cv2
+import hydra
 from digit_interface.digit import Digit
 from digit_interface.digit_handler import DigitHandler
 
@@ -38,13 +41,51 @@ class Digits:
         frame2 = self.d2.get_frame()
         return frame1, frame2
 
+    def get_serials(self):
+        return self.serial1, self.serial2
+
+
+class _DigitsThread(threading.Thread):
+    def __init__(self, digits_cfg):
+        threading.Thread.__init__(self)
+        self.digit = hydra.utils.instantiate(digits_cfg)
+        self.flag_exit = False
+        self.frame1 = None
+        self.frame2 = None
+
+    def run(self):
+        while not self.flag_exit:
+            self.frame1, self.frame2 = self.digit.get_image()
+
+
+class ThreadedDigits:
+    def __init__(self, digits_cfg):
+        self._digit_thread = _DigitsThread(digits_cfg)
+        self._digit_thread.start()
+
+    def get_image(self):
+        while self._digit_thread.frame1 is None or self._digit_thread.frame2 is None:
+            time.sleep(0.01)
+        frame1 = self._digit_thread.frame1.copy()
+        frame2 = self._digit_thread.frame2.copy()
+        return frame1, frame2
+
+    def get_serials(self):
+        return self._digit_thread.digit.get_serials()
+
+    def __del__(self):
+        self._digit_thread.flag_exit = True
+        self._digit_thread.join()
+
 
 def test_digit():
-    d = Digits("D00020", "D00010", "QVGA", "30fps")
+    from omegaconf import OmegaConf
+    cfg = OmegaConf.load("../conf/tactile_sensing/digits.yaml")
+    d = ThreadedDigits(cfg)
     while True:
         frame1, frame2 = d.get_image()
-        cv2.imshow(f"Digit View {d.serial1}", frame1)
-        cv2.imshow(f"Digit View {d.serial2}", frame2)
+        cv2.imshow(f"Digit View {d.get_serials()[0]}", frame1)
+        cv2.imshow(f"Digit View {d.get_serials()[1]}", frame2)
         if cv2.waitKey(1) == 27:
             break
     cv2.destroyAllWindows()
