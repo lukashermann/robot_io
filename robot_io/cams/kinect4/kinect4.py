e9f9dea40881f5bdbb80891359c4fd8d190f88c3
nematoli <nematoli@cs.uni-freiburg.de>
2021-08-09 14:34:51 +0200

    detects which kinect4 device (out of two) is connected and loads its corresponding parameters

--- robot_io/cams/kinect4/kinect4.py
+++ robot_io/cams/kinect4/kinect4.py
@@ -8,51 +8,80 @@ import threading
 from pathlib import Path
 
 from robot_io.cams.camera import Camera
+from contextlib import redirect_stdout
+import io
 
 
 class Kinect4(Camera):
     def __init__(self,
                  device=0,
                  align_depth_to_color=True,
-                 config_path='config/config_kinect4_1080p.json',
-                 params_file_path="config/kinect4_params_1080p.npz",
+                 resolution='1080p',
                  undistort_image=True,
                  resize_resolution=None,
                  crop_coords=None,
                  fps=30):
-        resolution, config, data = self.load_config(config_path, params_file_path)
-        super().__init__(resolution=resolution, crop_coords=crop_coords, resize_resolution=resize_resolution, name="azure_kinect")
+
+        config_path = "config/config_kinect4_{}.json".format(resolution)
+        config = self.load_config_path(config_path)
+
+        self.sensor = o3d.io.AzureKinectSensor(config)
+        f = io.StringIO()
+        with redirect_stdout(f):
+            if not self.sensor.connect(device):
+                raise RuntimeError('Failed to connect to sensor')
+        device_info = f.getvalue()
+        kinect_instance = self.get_kinect_instance(device_info)
+        params_file_path = "config/kinect4{}_params_{}.npz".format(kinect_instance, resolution)
+        resolution, data = self.load_config_data(params_file_path)
+
+        super().__init__(resolution=resolution, crop_coords=crop_coords, resize_resolution=resize_resolution,
+                         name="azure_kinect")
         self.dist_coeffs = data['dist_coeffs']
         self.camera_matrix = data['camera_matrix']
         self.projection_matrix = data['projection_matrix']
         self.intrinsics = data['intrinsics'].item()
         self.map1, self.map2 = cv2.initUndistortRectifyMap(self.camera_matrix, self.dist_coeffs, R=np.eye(3), \
-                newCameraMatrix=self.camera_matrix, size=(self.intrinsics['width'], self.intrinsics['height']), m1type= cv2.CV_16SC2)
+                                                           newCameraMatrix=self.camera_matrix,
+                                                           size=(self.intrinsics['width'], self.intrinsics['height']),
+                                                           m1type=cv2.CV_16SC2)
         self.device = device
         self.align_depth_to_color = align_depth_to_color
         self.config_path = config_path
         self.undistort_image = undistort_image
         self.fps = fps
-
         self.align_depth_to_color = align_depth_to_color
-        self.sensor = o3d.io.AzureKinectSensor(config)
-        if not self.sensor.connect(device):
-            raise RuntimeError('Failed to connect to sensor')
 
-    def load_config(self, config_path, params_file_path):
-        data = np.load((Path(__file__).parent / params_file_path).as_posix(), allow_pickle=True)
+    def load_config_path(self, config_path):
         if config_path is not None:
             full_path = (Path(__file__).parent / config_path).as_posix()
             config = o3d.io.read_azure_kinect_sensor_config(full_path)
         else:
             config = o3d.io.AzureKinectSensorConfig()
+
+        return config
+
+    def load_config_data(self, params_file_path):
+        data = np.load((Path(__file__).parent / params_file_path).as_posix(), allow_pickle=True)
         if "1080" in params_file_path:
             resolution = (1920, 1080)
         elif "720" in params_file_path:
             resolution = (1280, 720)
         else:
             raise ValueError
-        return resolution, config, data
+
+        return resolution, data
+
+    def get_kinect_instance(self, device_info):
+        serial_number = [int(s) for s in device_info.splitlines()[2].split() if s.isdigit()][0]
+        if serial_number == 232793712:
+            kinect_instance = 'a'
+        elif serial_number == 172402712:
+            kinect_instance = 'b'
+        else:
+            raise ValueError
+
+        return kinect_instance
 
     def get_intrinsics(self):
         return self.intrinsics
@@ -79,7 +108,7 @@ class Kinect4(Camera):
         return rgb, depth
 
 
-def test_camera():
+def run_camera():
     # cam = Kinect4(0, crop_coords=(301, 623, 516, 946), resize_resolution=(200, 150))
     cam = Kinect4(0)
     while True:
@@ -90,4 +119,4 @@ def test_camera():
 
 
 if __name__ == "__main__":
-    test_camera()
+    run_camera()
