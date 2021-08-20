336249fc1cf8e0996e568a243153f7dfd54d3a4c
nematoli <nematoli@cs.uni-freiburg.de>
2021-08-20 11:29:34 +0200

    robot_env requests a dict for action. For now action dict keys are 'motion' and 'ref'

--- robot_io/control/robot_env.py
+++ robot_io/control/robot_env.py
@@ -37,16 +37,25 @@ class RobotEnv(gym.Env):
     def step(self, action):
         """
         Execute one action on the robot.
-        :param action: cartesian action tuple (position, orientation, gripper_action)
+        :param action: {"motion": (position, orientation, gripper_action), "ref": "rel"/"abs"}
+                       a dict with the key 'motion' which is a cartesian motion tuple
+        :              and the key 'ref' which specifies if the motion is absolute or relative
         :return: obs, reward, done, info
         """
         if action is None:
             return self._get_obs(), 0, False, {}
-        assert isinstance(action, tuple) and len(action) == 3
+        assert isinstance(action, dict) and len(action['motion']) == 3
 
-        target_pos, target_orn, gripper_action = action
-        target_pos = self._restrict_workspace(target_pos)
-        self.robot.move_async_cart_pos_abs_ptp(target_pos, target_orn)
+        target_pos, target_orn, gripper_action = action['motion']
+        ref = action['ref']
+
+        if ref is "abs":
+            target_pos = self._restrict_workspace(target_pos)
+            self.robot.move_async_cart_pos_abs_ptp(target_pos, target_orn)
+        elif ref is "rel":
+            self.robot.move_async_cart_pos_rel_ptp(target_pos, target_orn)
+        else:
+            raise ValueError
 
         if gripper_action == 1:
             self.robot.open_gripper()
