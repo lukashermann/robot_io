89d5d9d39f1093e570c1c25798ca97fec4535ce9
Oier Mees <oier.mees@gmail.com>
2021-08-28 12:39:14 +0200

    remove comment

--- robot_io/control/teleop_robot.py
+++ robot_io/control/teleop_robot.py
@@ -20,8 +20,6 @@ def main(cfg):
     while True:
         action, record_info = input_device.get_action()
         obs, _, _, _ = env.step(action)
-        # if action is None:
-        #     print("action is NONE!!!!!!!!!!!!!!")
         recorder.step(action, obs, record_info)
         env.render()
         fps.step()
