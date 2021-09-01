f85faaf5dc2e1f000ebdb0cba973c2a1d2e10c4d
Oier Mees <oier.mees@gmail.com>
2021-09-01 14:34:35 +0200

    fix saving correct camera matrices

--- robot_io/cams/camera_manager.py
+++ robot_io/cams/camera_manager.py
@@ -43,13 +43,31 @@ class CameraManager:
             camera_info["gripper_extrinsic_calibration"] = self.gripper_cam.get_extrinsic_calibration(robot_name)
             camera_info["gripper_intrinsics"] = self.gripper_cam.get_intrinsics()
         if self.static_cam is not None:
-            camera_info["static_extrinsic_calibration"] = self.gripper_cam.get_extrinsic_calibration(robot_name)
-            camera_info["static_intrinsics"] = self.gripper_cam.get_intrinsics()
+            camera_info["static_extrinsic_calibration"] = self.static_cam.get_extrinsic_calibration(robot_name)
+            camera_info["static_intrinsics"] = self.static_cam.get_intrinsics()
         np.savez("camera_info.npz", **camera_info)
+
+    def normalize_depth(self, img):
+        img_mask = img == 0
+        istats = (np.min(img[img > 0]), np.max(img))
+        imrange = (img.astype("float32") - istats[0]) / (istats[1] - istats[0])
+        imrange[img_mask] = 0
+        imrange = 255.0 * imrange
+        imsz = imrange.shape
+        nchan = 1
+        if len(imsz) == 3:
+            nchan = imsz[2]
+        imgcanvas = np.zeros((imsz[0], imsz[1], nchan), dtype="uint8")
+        imgcanvas[0: imsz[0], 0: imsz[1]] = imrange.reshape((imsz[0], imsz[1], nchan))
+        return imgcanvas
 
     def render(self):
         if "rgb_gripper" in self.obs:
             cv2.imshow("rgb_gripper", self.obs["rgb_gripper"][:, :, ::-1])
+        if "depth_gripper" in self.obs:
+            depth_img_gripper = self.normalize_depth(self.obs["depth_gripper"])
+            depth_img_gripper = cv2.applyColorMap(depth_img_gripper, cv2.COLORMAP_JET)
+            cv2.imshow("depth_gripper", depth_img_gripper)
         if "rgb_static" in self.obs:
             cv2.imshow("rgb_static", self.obs["rgb_static"][:, :, ::-1])
         cv2.waitKey(1)
