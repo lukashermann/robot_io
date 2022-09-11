0e5ff6620e2d6a3e7ac5bea9c521bbd6c22d90d3
Jessica Borja <jessicalizethborja@hotmail.com>
2022-09-11 12:02:05 -0700

    resize resolution

--- robot_io/cams/threaded_camera.py
+++ robot_io/cams/threaded_camera.py
@@ -26,7 +26,7 @@ class ThreadedCamera:
         return rgb, depth
 
     def get_resize_res(self):
-        return self._camera_thread.camera.resize_res
+        return self._camera_thread.camera.resize_resolution
 
     def get_intrinsics(self):
         return self._camera_thread.camera.get_intrinsics()
